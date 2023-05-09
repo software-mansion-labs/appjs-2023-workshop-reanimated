ea2437379d5615436d15a066d6fe61dbc2c4e4b9
catalinmiron <mironcatalin@gmail.com>
2023-05-09 10:56:00 +0200

    Finish scroll animation walkthrough

--- src/lessons/ScrollAnimation/steps/boilerplate.tsx
+++ src/lessons/ScrollAnimation/steps/boilerplate.tsx
@@ -2,7 +2,7 @@ import { ContactsListHeader } from '@components/ContactsListHeader'
 import { ContactsListItem } from '@components/ContactsListItem'
 import { Container } from '@components/Container'
 import { alphabet, contacts } from '@lib/mock'
-import { clamp, hitSlop } from '@lib/reanimated'
+import { hitSlop } from '@lib/reanimated'
 import { colorShades, layout } from '@lib/theme'
 import { useMemo } from 'react'
 import { SectionList, StyleSheet, View } from 'react-native'
@@ -10,51 +10,19 @@ import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import Animated, {
   Extrapolate,
   interpolate,
-  measure,
-  runOnJS,
-  runOnUI,
-  SharedValue,
-  useAnimatedRef,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
   withSpring,
-  withTiming,
 } from 'react-native-reanimated'
 import sectionListGetItemLayout from 'react-native-section-list-get-item-layout'
 
 type AlphabetLetterProps = {
   index: number
   letter: string
-  scrollableIndex: SharedValue<number>
 }
 
-const AlphabetLetter = ({
-  index,
-  letter,
-  scrollableIndex,
-}: AlphabetLetterProps) => {
-  const posY = useSharedValue(0)
-  const styles = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(
-        scrollableIndex.value,
-        [index - 1, index, index + 1],
-        [0.5, 1, 0.5],
-        Extrapolate.CLAMP,
-      ),
-      transform: [
-        {
-          scale: interpolate(
-            scrollableIndex.value,
-            [index - 2, index, index + 2],
-            [1, 1.5, 1],
-            Extrapolate.CLAMP,
-          ),
-        },
-      ],
-    }
-  })
+const AlphabetLetter = ({ index, letter }: AlphabetLetterProps) => {
   return (
     <Animated.View
       style={[
@@ -64,7 +32,6 @@ const AlphabetLetter = ({
           justifyContent: 'center',
           flexDirection: 'row',
         },
-        styles,
       ]}
     >
       <Animated.Text
@@ -86,8 +53,6 @@ const AlphabetLetter = ({
 export function ScrollAnimationLesson() {
   const y = useSharedValue(0)
   const isInteracting = useSharedValue(false)
-  const scrollableIndex = useSharedValue(0)
-  const activeScrollIndex = useSharedValue(0)
   const knobScale = useDerivedValue(() => {
     return withSpring(isInteracting.value ? 1 : 0)
   })
@@ -99,61 +64,17 @@ export function ScrollAnimationLesson() {
     })
   }, [])
 
-  const alphabetRef = useAnimatedRef<View>()
-
-  const snapIndicatorTo = (index: number) => {
-    runOnUI(() => {
-      'worklet'
-
-      if (scrollableIndex.value === index || isInteracting.value) {
-        return
-      }
-
-      const alphabetLayout = measure(alphabetRef)
-      if (!alphabetLayout) {
-        return
-      }
-      const snapBy =
-        (alphabetLayout.height - layout.knobSize) / (alphabet.length - 1)
-      const snapTo = index * snapBy
-      y.value = withTiming(snapTo)
-      scrollableIndex.value = withTiming(index)
-    })()
-  }
-
   const panGesture = Gesture.Pan()
     .averageTouches(true)
     .onBegin(() => {
       isInteracting.value = true
     })
     .onChange((ev) => {
-      const alphabetLayout = measure(alphabetRef)
-      if (!alphabetLayout) {
-        return
-      }
-      y.value = clamp(
-        (y.value += ev.changeY),
-        alphabetLayout.y, // take into account the knob size
-        alphabetLayout.height - layout.knobSize,
-      )
-      // This is snapTo by the same interval. This will snap to the nearest
-      // letter based on the knob position.
-      const snapBy =
-        (alphabetLayout.height - layout.knobSize) / (alphabet.length - 1)
-
-      scrollableIndex.value = y.value / snapBy
-      const snapToIndex = Math.round(scrollableIndex.value)
-
-      // Ensure that we don't trigger scroll to the same index.
-      if (snapToIndex === activeScrollIndex.value) {
-        return
-      }
-
-      // This is to avoid triggering scrolling to the same index.
-      activeScrollIndex.value = snapToIndex
+      // take into account the knob size
+      y.value += ev.changeY
     })
     .onEnd(() => {
-      runOnJS(snapIndicatorTo)(activeScrollIndex.value)
+      y.value = withSpring(0)
     })
     .onFinalize(() => {
       isInteracting.value = false
@@ -209,7 +130,6 @@ export function ScrollAnimationLesson() {
             />
           </GestureDetector>
           <View
-            ref={alphabetRef}
             style={{
               transform: [{ translateX: -layout.indicatorSize / 4 }],
               flex: 1,
@@ -220,12 +140,7 @@ export function ScrollAnimationLesson() {
           >
             {[...Array(alphabet.length).keys()].map((i) => {
               return (
-                <AlphabetLetter
-                  key={i}
-                  letter={alphabet.charAt(i)}
-                  index={i}
-                  scrollableIndex={scrollableIndex}
-                />
+                <AlphabetLetter key={i} letter={alphabet.charAt(i)} index={i} />
               )
             })}
           </View>
